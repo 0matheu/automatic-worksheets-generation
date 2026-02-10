cc8dbddaf513f8bda4ab60a8b0e6b8b8d03af653
matheus lisboa <matheus.lis.san@gmail.com>
2026-02-10 18:58:21 -0300

    adding the code

---
+++ README.md
@@ -0,0 +1,5 @@
+# Code and dataset for the automatic-worksheet-generation
+
+- The code and the dataset can be found at: experimento3-final/handmade_html_dataset/english/train
+- The script used for train can be found at: experimento3-final/01-finetuning-gpt-41.ipynb
+- The evaluation with the volunteers can be found at: experimento4-voluntarios/evaluation.ipynb
---
+++ experimento4-voluntarios/evaluations/sillas/script_verify_answers.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import glob
+import os
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+
+for file in glob.iglob(f"{this_dir}/**/*.csv", recursive=True):
+
+    print(f"Processing file: {file}")
+    data = pd.read_csv(file)
+
+    try:
+        data = data[data["Resposta"].isnull()]
+    except:
+        data = data[data["resposta"].isnull()]
+
+    if data.shape[0] != 0:
+        for index, row in data.iterrows():
+            print(
+                f"ERROR :: Arquivo {file} - Está sem resposta na pergunta {index + 1}"
+            )
